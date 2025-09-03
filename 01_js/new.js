ac4017794d234d08ed99039ad035711177c81eea
Saurav-study <sauravchoudhary8789@gmail.com>
2025-09-03 10:01:15 +0530

    Java_Script

---
+++ 01_js/new.js
@@ -0,0 +1,4 @@
+
+
+console.log("Hello world")
+console.log("code is runing...")
---
+++ 01_js/script.js
@@ -0,0 +1,14 @@
+alert("Hello World");
+
+var a=prompt("Enter your nos")
+var isTrue=confirm("Are u sure u want to leave the page and blast your device")
+if(isTrue){
+    console.log("computer is blasting")
+}
+else{
+    console.log("device is ok")
+}
+console.log("your nos is " + a)
+
+document.title="Hey I am Good "
+document.body.style.backgroundColor="red"
