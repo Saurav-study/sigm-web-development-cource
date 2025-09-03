b71ec9588432f12e71191d310e98b4e364c60970
Saurav-study <sauravchoudhary8789@gmail.com>
2025-09-03 11:25:53 +0530

    Java_script

---
+++ 02_js/script.js
@@ -0,0 +1,33 @@
+console.log("Hey ")
+let a= 5;
+a=a+1
+let b= 6;
+let c="Saurav"
+console.log(a+b)
+console.log(typeof a,typeof b, typeof c)
+{
+    let a=66
+    console.log(a)
+}
+console.log(a)
+// const a1=6
+// a1=a1+9; This is not allowed because a1 is constsnt
+
+let x = "Saurav bhai";
+let Y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null
+console.log(x,Y,z,p,q,r)
+console.log(typeof x,typeof Y,typeof z,typeof p,typeof q,typeof r)
+
+//talking about object 
+let o = { name : "surav",
+    "Job code ": 3300
+}
+console.log(o)
+o.salary="2000crores";
+console.log(o)
+
+
